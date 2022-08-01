99f40d088937a2958c7a0c203d2f4c457a573eee
Sediq <sediqabdullahi04@gmail.com>
2022-08-01 11:29:23 +0100

    initial commmit

---
+++ api.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+  app = express(),
+  dotenv = require("dotenv").config(),
+  starter = require("./db"),
+  Router = require("./modules/routes");
+
+app.use(Router);
+starter(
+  app,
+  process.env.DB_PORT
+  // "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg"
+);
