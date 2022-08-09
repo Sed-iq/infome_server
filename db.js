b37a630196090bbbe0e42f55f5008ec18ae59d3f
Sediq <sediqabdullahi04@gmail.com>
2022-08-09 19:48:05 +0100

    cinie

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ module.exports = (server, url) => {
   mongoose.connect(url, (err) => {
     if (!err) server.listen(process.env.PORT, console.log("SERVER IS RUNNING"));
     else {
-      server.listen(
-        process.env.PORT,
-        console.error("Server running on safe mode database error \n")
-      );
+     trow err
     }
   });
 };
