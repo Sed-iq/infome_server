4a86388836fe02dd1bc5b2207b838653b017937b
Sediq <sediqabdullahi04@gmail.com>
2022-08-09 15:34:00 +0100

    duhuids

--- api.js
+++ api.js
@@ -1,15 +1,18 @@
-const express = require("express"),
-  app = express(),
-  mongoose = require("mongoose");
-// Router = require("./modules/routes");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+let PORT =
+  "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg";
 
-// app.use(Router);
-app.get("/", (req, res) => res.send("HEY"));
+app.use((req, res) => {
+  res.json({ name: "sediq" });
+});
+
 mongoose
-  .connect(
-    "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg"
-  )
+  .connect(PORT)
   .then(() => {
     app.listen(process.env.PORT, () => console.log("Server running"));
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
