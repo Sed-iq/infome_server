b475828bf0213bc6c389ed7396a08c874a1c0b0e
Sediq <sediqabdullahi04@gmail.com>
2022-09-22 05:27:51 +0100

    rm

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Router = require("./modules/routes");
-const app = express();
-let PORT =
-  "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg";
+// const express = require("express");
+// const mongoose = require("mongoose");
+// // const Router = require("./modules/routes");
+// const app = express();
+// let PORT =
+//   "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg";
 
-mongoose
-  .connect(PORT)
-  .then(() => {
-    app.listen(process.env.PORT || 5000, () => console.log("Server running"));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// app.get("/", ({}, res) => {
+//   res.send("HOME PAGE");
+// });
 
-app.use(Router);
+// mongoose
+//   .connect(PORT)
+//   .then(() => {
+//     app.listen(process.env.PORT || 5000, () => console.log("Server running"));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// app.use(Router);
+
+let http = require("http");
+let server = http.createServer((req, res) => {
+  if ((req.url = "/")) res.end("<h1>Home Page</h1>");
+  else res.end("404");
+});
+server.listen(5000, console.log("Server is running"));
