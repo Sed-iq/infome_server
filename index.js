6d0d8bcf5c600ecbd4a85d44010faea6f5526390
Sediq <sediqabdullahi04@gmail.com>
2022-09-28 01:19:44 +0100

    testing cloudinary

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 // const Router = require("./modules/routes");
 const upload = require("./modules/uploadController");
 const imgsch = require("./modules/fakeimg");
 const bodyParser = require("body-parser");
+const cloudinary = require("cloudinary").v2;
 const path = require("path");
+const fs = require("fs");
 const app = express();
 let PORT =
   "mongodb+srv://Trustadmin:08126074692@cluster0.t9mbj.mongodb.net/TrustNg";
-//"mongodb://localhost/blog";
+// "mongodb://localhost/blog";
 app.use(bodyParser({ extended: true }));
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
+async function cloudinaryUploader(localfilepath) {
+  var mainFolderName = "main";
+
+  var filePathOnCloudinary = mainFolderName + "/" + localfilepath;
+
+  return cloudinary.uploader
+    .upload(localfilepath, { public_id: filePathOnCloudinary })
+    .then((result) => {
+      fs.unlinkSync(localfilepath);
+      return {
+        message: "Success",
+        url: result.url,
+      };
+    })
+    .catch((error) => {
+      // Remove file from local uploads folder
+      fs.unlinkSync(localfilepath);
+      return { message: "Fail" };
+    });
+}
+function buildSuccessMsg(urlList) {
+  return urlList;
+}
 app.get("/", ({}, res) => {
   res.send(
     `<h2>Upload File </h2> <form action="/file" enctype="multipart/form-data" method="POST"> <input type="file" name="file"> <br> <button>Upload</button> </form>`
   );
 });
-app.post("/file", upload.single("file"), (req, res) => {
+app.post("/file", upload.single("file"), async (req, res) => {
+  let local = req.file.path;
+
+  let result = await cloudinaryUploader(local);
+
+  let response = buildSuccessMsg([result.url]);
+
   let Img = new imgsch({
-    img: req.file.filename,
+    img: response[0],
   });
   Img.save()
     .then((data) => {
-      if (data) res.send(data);
+      if (data) res.json(data);
       else res.redirect("/");
     })
     .catch((err) => {
       res.json(err);
     });
 });
-app.get("/pic/:file", ({ params }, res) => {
-  imgsch
-    .findOne({ img: params.file })
-    .then((data) => {
-      if (data) res.sendFile(path.join(__dirname + `/uploads/${data.img}`));
-      else res.send(`404`);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+app.get("/pic/:file", ({ params }, res) => {});
 mongoose
   .connect(PORT)
   .then(() => {
